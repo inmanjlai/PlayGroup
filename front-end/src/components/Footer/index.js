82e43c9bd5b8f01c19173304665c8b51995257a7
Ismail <inmanjlai@hotmail.com>
2021-09-11 09:34:03 -0400

    changes to css

--- front-end/src/components/Footer/index.js
+++ front-end/src/components/Footer/index.js
@@ -3,34 +3,36 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <footer>
-            <div className="get-started">
-                <h2>Create your own PlayGroup.</h2>
-                <button>Get Started</button>
-            </div>
+            <div className="container">
+                <div className="get-started">
+                    <h2>Create your own PlayGroup.</h2>
+                    <button>Get Started</button>
+                </div>
 
 
-            <div className="account-list">
-                <h3>Your Account</h3>
-                <ul>
-                    <li>Sign up</li>
-                    <li>Log in</li>
-                </ul>
-            </div>
+                <div className="account-list">
+                    <h3>Your Account</h3>
+                    <ul>
+                        <li>Sign up</li>
+                        <li>Log in</li>
+                    </ul>
+                </div>
 
-            <div className="discover-list">
-                <h3>Discover</h3>
-                <ul>
-                    <li>Groups</li>
-                    <li>Events</li>
-                </ul>
-            </div>
+                <div className="discover-list">
+                    <h3>Discover</h3>
+                    <ul>
+                        <li>Groups</li>
+                        <li>Events</li>
+                    </ul>
+                </div>
 
-            <div className="about-list">
-                <h3>PlayGroup</h3>
-                <ul>
-                    <li>About</li>
-                    <li>Made by Ismail N Manjlai</li>
-                </ul>
+                <div className="about-list">
+                    <h3>PlayGroup</h3>
+                    <ul>
+                        <li>About</li>
+                        <li>Made by Ismail N Manjlai</li>
+                    </ul>
+                </div>
             </div>
         </footer>
     )
